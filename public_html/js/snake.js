3f94e383ae13a3dfbb1ecac49ff5a9fa12dd289f
Ki Hwan Chong <joelkh784@gmail.com>
2014-10-23 11:25:42 -0700

    no message

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -176,4 +176,5 @@ function checkWallCollisions(snakeHeadX, snakeHeadY) {
     if(snakeHeadX * snakeSize >= screenWidth || snakeHeadX * snakeSize < 0) {
         console.log(WallCollisions);
     }
+    
 }
